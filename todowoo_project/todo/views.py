526afb13983dcf46e8361431c7773f2649754697
Reagan Ronald <reaganronashi@gmail.com>
2023-04-08 23:33:07 +0300

    views.py

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import user
 # Create your views here.
+
+
 def signuser(request):
-    
-    return render(request, 'todo/signuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+    return render(request, 'todo/signuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2]:
+            user =  User.object.create_user(request.POST['password1'])
+            user.save()
+            
+        
+     
      
