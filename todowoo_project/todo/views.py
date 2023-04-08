169b93dea218f2c3cb9581c3411825bca826638d
Reagan Ronald <reaganronashi@gmail.com>
2023-04-08 18:28:59 +0300

    views mod

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signuser(request):
     
-    return render(request, 'todo/signuser.html')
+    return render(request, 'todo/signuser.html', {'form':UserCreationForm()})
      
